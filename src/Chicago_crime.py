f2bb27dbec6a3e9027cb3d7c279ef188e904fc9e
Nancy <nancy@nagelzoo.net>
2019-10-17 13:51:06 -0600

    regression model

--- src/Chicago_crime.py
+++ src/Chicago_crime.py
@@ -12,6 +12,9 @@ import geopandas as gpd
 from shapely.geometry import Point, Polygon
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from numpy.linalg import svd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
 #pd.options.display.max_colwidth = 50
 ##########pulling sample from large file and saving to csv
@@ -39,44 +42,47 @@ chicagocrimessub = chicagocrimes.where(chicagocrimes['Year'] >= 2010)
 # chicagocrimessub['Arrest'] = chicagocrimessub['Arrest'].astype(int)
 
 #####prepare data matrix
-Xmatrix = chicagocrimessub.drop(['ID', 'Case Number', 'Block', 'Date', 'IUCR', 'Primary Type', 'Description', 'Location Description', 'Police Districts', 'Police Beats','FBI Code', 'X Coordinate', 'Y Coordinate', 'Updated On', 'Location', 'Historical Wards 2003-2015', 'Boundaries - ZIP Codes'], axis = 1)
-Xmatrix = Xmatrix.astype('float')
+Xmatrix = chicagocrimessub.drop(['ID','Primary Type', 'Community Areas', 'Description', 'Case Number', 'Date', 'Block', 'IUCR', 'Location Description', 'Police Districts', 'Police Beats','FBI Code', 'X Coordinate', 'Y Coordinate', 'Updated On', 'Location', 'Wards', 'Historical Wards 2003-2015', 'Boundaries - ZIP Codes'], axis = 1)
+#Xmatrix = Xmatrix.astype('float')
 Xmatrixnona = Xmatrix.dropna()
 
 ######Perform EDA
 
 #chicagocrimes.isnull().sum(axis=0)
 
-# ptvaluesbyyear = chicagocrimessub.groupby("Year")['Primary Type'].value_counts() 
-# ptvalues = chicagocrimessub['Primary Type'].value_counts()
-# ptvaluesarray = np.array(ptvalues)
+#ptvaluesbyyear = chicagocrimessub.groupby("Year")['Primary Type'].value_counts() 
+#ptvalues = chicagocrimessub.groupby('Primary Type')['Arrest'].value_counts()
+#ptvaluesarray = np.array(ptvalues)
 
-# ptdf = pd.DataFrame(ptvaluesbyyear)
+#ptdf = pd.DataFrame(ptvaluesbyyear)
+#ptdf2 = pd.DataFrame(ptvalues)
 
 years = chicagocrimessub['Year'].unique()
-primtypes = chicagocrimessub['Primary Type'].unique()
+#primtypes = chicagocrimessub['Primary Type'].unique()
+
 # ##convert dates to a day of week field
-datetodatetime = pd.to_datetime(chicagocrimessub['Date'])
+#datetodatetime = pd.to_datetime(chicagocrimessub['Date'])
 
 chicagopops = {'2010': 2695598, '2011': 2707120, '2012': 2700800, '2013': 2710000, '2014': 2715000, '2015': 2723000, '2016': 2720546, '2017': 2722586, '2018': 2705994, '2019': 2695000}
 
 weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-dayofweek = datetodatetime.dt.weekday
+#dayofweek = datetodatetime.dt.weekday
 #dayofweekasstring = weekDays[dayofweek]
 
 ###########mapping
-#street_map = gpd.read_file('data/geo_export_33ca7ae0-c469-46ed-84da-cc7587ccbfe6.shp')
-# Xmatrixmap = Xmatrixnona.drop(['Ward', 'District', 'Beat', 'Community Area', 'Community Areas', 'Census Tracts', 'Zip Codes', 'Wards'], axis=1)
+# street_map = gpd.read_file('data/geo_export_33ca7ae0-c469-46ed-84da-cc7587ccbfe6.shp')
+# Xmatrixmap = Xmatrixnona.drop(['Ward', 'District', 'Beat', 'Community Area', 'Community Areas', 'Census Tracts', 'Zip Codes'], axis=1)
 # Xmatrixmap2010=Xmatrixmap[Xmatrixmap['Year'] == 2010]
 # Xmatrixmap2018=Xmatrixmap[Xmatrixmap['Year'] == 2018]
 # crs = {"init": 'epsg:4326'}
 # geometry = [Point(xy) for xy in zip(Xmatrixmap2010["Longitude"], Xmatrixmap2010["Latitude"])]
 # geometry18 = [Point(xy) for xy in zip(Xmatrixmap2018["Longitude"], Xmatrixmap2018["Latitude"])]
 # #Xmatrixmap = Xmatrixnona['Year','Arrest', 'Domestic', 'Latitude', 'Longitude']
-
 # geo_df = gpd.GeoDataFrame(Xmatrixmap2010, crs=crs, geometry=geometry)
 # geo_df18 = gpd.GeoDataFrame(Xmatrixmap2018, crs=crs, geometry = geometry18)
 
+# ptdfarrest = geo_df.groupby('Primary Type')['Arrest']
+# ptarrestdf = pd.DataFrame(ptdfarrest)
 
 def geo_maps(df, measure, year):
     fig,ax = plt.subplots(figsize = (15, 15))
@@ -92,11 +98,12 @@ def geo_maps(df, measure, year):
 
 # fig,ax = plt.subplots(figsize = (15, 15))
 # street_map.plot(ax = ax, alpha = .4, color='grey')
-# geo_df[geo_df['Domestic'] == 1].plot(ax=ax, markersize = 20, color = 'red', marker = "x", label='DV')
-# plt.title('Domestic Violence Calls by Location 2010')
+# geo_df['Primary Type'].values.plot(ax=ax)
+# plt.title('Calls by Primary Type 2010')
 # plt.legend(prop={'size': 15})
 # plt.show()
 
+###geomap of DV calls 
 # fig, ax = plt.subplots(figsize=(15,15))
 # street_map.plot(ax = ax, alpha = .4, color='grey')
 # geo_df18[geo_df18['Domestic'] == 1].plot(ax=ax, markersize = 20, color = 'red', marker = "x", label='DV')
@@ -128,14 +135,6 @@ def plot_eda_lines(measure, y):
     plt.title(year)
     return
 
-def plot_death_rates(data, measure):
-    plt.rc('xtick', labelsize=6) 
-    plt.rc('ytick', labelsize=6)
-    fig, ax = plt.subplots(figsize=(10, 8))
-    plt.barh(data['Location'], data[measure])
-    plt.title(measure)
-    return
-
 # lists = sorted(chicagopops.items()) # sorted by key, return a list of tuples
 # x, y = zip(*lists) # unpack a list of pairs into two tuples
 # plt.plot(x, y)
@@ -143,10 +142,10 @@ def plot_death_rates(data, measure):
 # plt.title('City of Chicago Population by Year')
 # plt.show()
 
-fig,ax = plt.subplots(figsize = (15, 15))
-ax.plot(arrestsbyyear.index,arrestsbyyear.values, color='red')
-ax.plot(domesticbyyear.index,domesticbyyear.values, color = 'blue')
-plt.title('Events by Year')
+# fig,ax = plt.subplots(figsize = (15, 15))
+# ax.plot(arrestsbyyear.index,arrestsbyyear.values, color='red')
+# ax.plot(domesticbyyear.index,domesticbyyear.values, color = 'blue')
+# plt.title('Arrests and Domestic Events by Year')
 
 # fig,ax = plt.subplots()
 # for primtype in primtypes:
@@ -171,23 +170,6 @@ plt.title('Events by Year')
 #     plt.legend()
 
 
-###########normalize the data
-# beatmean=np.mean(Xmatrixnona['Beat'])
-# beatstd=np.std(Xmatrixnona['Beat'])
-# Xmatrixnona['beatnorm'] = (Xmatrixnona['Beat'] - beatmean)/beatstd
-# districtmean=np.mean(Xmatrixnona['District'])
-# districtstd=np.std(Xmatrixnona['District'])
-# Xmatrixnona['districtnorm'] = (Xmatrixnona['District'] - districtmean)/districtstd
-# wardmean=np.mean(Xmatrixnona['Ward'])
-# wardstd=np.std(Xmatrixnona['Ward'])
-# Xmatrixnona['wardnorm'] = (Xmatrixnona['Ward'] - wardmean)/wardstd
-# Xmatrixnona['commareanorm'] = (Xmatrixnona['Community Area'] - np.mean(Xmatrixnona['Community Area']))/np.std(Xmatrixnona['Community Area'])
-# Xmatrixnona['yearnorm'] = (Xmatrixnona['Year'] - np.mean(Xmatrixnona['Year']))/np.std(Xmatrixnona['Year'])
-# Xmatrixnona['censusnorm'] = (Xmatrixnona['Census Tracts'] - np.mean(Xmatrixnona['Census Tracts']))/np.std(Xmatrixnona['Census Tracts'])
-# Xmatrixnona['zipnorm'] = (Xmatrixnona['Zip Codes'] - np.mean(Xmatrixnona['Zip Codes']))/np.std(Xmatrixnona['Zip Codes'])
-# Xmatrixnona['latnorm'] = (Xmatrixnona['Latitude'] - np.mean(Xmatrixnona['Latitude']))/np.std(Xmatrixnona['Latitude'])
-# Xmatrixnona['longnorm'] = (Xmatrixnona['Longitude'] - np.mean(Xmatrixnona['Longitude']))/np.std(Xmatrixnona['Longitude'])
-
 ##########plot
 ##plot corr heatmap
 # f = plt.figure(figsize=(19, 15))
@@ -201,37 +183,61 @@ plt.title('Events by Year')
 #pd.plotting.scatter_matrix(Xmatrixnona)
 #plt.show()
 
-######SVD
-
-# U, sigma, VT = svd(Xmatrixnona, full_matrices=False, compute_uv=True)
-# # Check if U and VT matrices are column orthogonal
-# # If the matrix is orthogonal, it's dot product with itself should give
-# # the identity matrix (inline with itself (1), orthogonal with all the rest (0))
-
-# print("Checking U:")
-# print(np.dot(U.T,U).round(1))
-# print("\nChecking V:")
-# print(np.dot(VT.T,VT).round(1))
-
-# print("\nU matrix:")
-# print("Relates Users (rows) to latent features (columns) based on magnitude of values in matrix.\n")
-# print(U.round(2))
-
-# print("S matrix")
-# print(sigma.shape) # these are just the diagonal values of the singular values matrix
-# print("The latent feature singular values. The (singular value)^2 is\n"
-#       "is the eigenvalue.")
-# print("\nThe singular values:")
-# print(sigma.round(2)) # these are just the diagonal values of the singular values matrix
-# print("\nThe singular values matrix, S:")
-# sigma_m = sigma * np.eye(len(sigma))
-# print(sigma_m.round(2))
-
 ###############VIF to reduce features
+#Xmatrixnona=Xmatrixnona.drop('Domestic')
 # vif = pd.DataFrame()
 # vif["VIF Factor"] = [variance_inflation_factor(Xmatrixnona.to_numpy(), i) for i in range(Xmatrixnona.shape[1])]
 # vif["features"] = Xmatrixnona.columns
 # vif.round(1)
+
+##########logistic regression
+X = Xmatrixnona.drop(['Domestic', 'Beat', 'District', 'Community Area', 'Year', 'Latitude', 'Longitude', 'Zip Codes'], axis=1)
+y = Xmatrixnona['Domestic'] 
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0, stratify = y) 
+
+logistic_regression= LogisticRegression(class_weight='balanced')
+model = logistic_regression.fit(X_train,y_train)
+y_pred=logistic_regression.predict(X_test)
+print(model)
+
+coeffs = pd.DataFrame(zip(X.columns, model.coef_))
+print('Coeffs:', coeffs)
+
+confusion_matrix = pd.crosstab(y_test, y_pred, rownames=['Actual'], colnames=['Predicted'])
+f, ax = plt.subplots(1, figsize=(10, 8))
+
+sns.set(font_scale=2)
+plt.title('Confusion Matrix for Logistic Regression')
+sns.heatmap(confusion_matrix, annot=True, ha='center', va='center', fmt="d", linewidths=.5, ax=ax)
+
+print('Accuracy: ',metrics.accuracy_score(y_test, y_pred))
+# results = confusion_matrix(y_test, y_pred) 
+# print('Confusion Matrix :')
+# print(results)  
+# print('Report : ')
+# print(classification_report(y_test, y_pred)) 
+
+FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)  
+FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
+TP = np.diag(confusion_matrix)
+TN = confusion_matrix.values.sum() - (FP + FN + TP)
+
+# Sensitivity, hit rate, recall, or true positive rate
+TPR = TP/(TP+FN)
+# Specificity or true negative rate
+TNR = TN/(TN+FP) 
+# Precision or positive predictive value
+PPV = TP/(TP+FP)
+# Negative predictive value
+NPV = TN/(TN+FN)
+# Fall out or false positive rate
+FPR = FP/(FP+TN)
+# False negative rate
+FNR = FN/(TP+FN)
+# False discovery rate
+FDR = FP/(TP+FP)
+
+# Overall accuracy
 
 if __name__ == '__main__':  
 
